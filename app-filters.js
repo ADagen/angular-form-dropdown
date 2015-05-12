a6282f485c36843dacce8e6fed7d7076bcfdf656
Андрей Бодров <dagen-niger@users.noreply.github.com>
2015-05-12 16:49:20 +0300

    fix for "All" options

--- app-filters.js
+++ app-filters.js
@@ -2,6 +2,10 @@
 	.filter('filterSelected', ['App.currency', function($currency) {
 		var filterSelected = function($base, selects) {
 			if (!$base.$resolved) { return []; }
+			
+			if (selects.selected1 == 99) {
+				return $base.base;
+			}
 		
 			var pair = $currency.getPair(selects.selected1, selects.selected2);
 			
